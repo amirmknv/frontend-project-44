f6a9b8a72641ae8bc7c43f223298ac42558341e6
amirmknv <amir.mukanov@gmail.com>
2023-11-03 22:21:53 +0800

    add bin/brain-even.js, src/cli.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
+
+import {userName} from "../src/cli.js";
+import readlineSync from 'readline-sync';
+
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-console.log("Question" + 
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log("Question: " + randomNumber);
+
+const answer = readlineSync.question("Your answer: ");
+
