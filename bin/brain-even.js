9ce4c83571e608c886844e5ea29707b620f1093a
amirmknv <amir.mukanov@gmail.com>
2023-11-04 16:00:36 +0800

    brain-even.js, src/games/even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,36 +1,4 @@
 #!/usr/bin/env node
 
-import {userName} from "../src/cli.js";
-import readlineSync from 'readline-sync';
+import {gameResult} from ".. src/games/even.js"; 
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log("Question: " + randomNumber);
-
-const answer = readlineSync.question("Your answer: ");
-
-const evenNum = (randomNumber % 2 === 0);
-const oddNum = (randomNumber % 2 !== 0);
-
-const findNumber = (randomNumber) => {
-
-	while (randomNumber = evenNum) {
-		if (answer === "yes") {
-		return "Correct!"
-		};
-		else {
-		return "Let's try again, " + userName;
-		};
-	};
-
-	while (randomNumber = oddNum) {
-		if (answer === "yes") {
-                return "Correct!"
-                };
-                else {
-                return "Let's try again, " + userName;
-	};
-};
-
-console.log(findNumber);
---
+++ src/games/even.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+import {userName} from "../src/cli.js";
+import readlineSync from 'readline-sync';
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log("Question: " + randomNumber);
+
+const answer = readlineSync.question("Your answer: ");
+
+const evenNum = (randomNumber % 2 === 0);
+const oddNum = (randomNumber % 2 !== 0);
+
+export const gameResult = (number) => {
+	if (number = evenNum) && (answer === "yes") {
+	console.log("Correct")
+	};
+	else if (number = oddNum) && (answer === "no") {
+	console.log("Correct")
+	};
+	else if (number = evenNum) && (answer === "no" {
+	console.log("'yes' is wrong answer ;(. Correct answer was 'no'. n/ Let's try again, " + userName) 
+	};
+	else if (number = oddNum) && (answer === "yes") {
+	console.log("'yes' is wrong answer ;(. Correct answer was 'no'. n/ Let's try again, " + userName)
+	};
+	
+};
+
+console.log(findNumber);
