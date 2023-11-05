5e31750e38b411171955574eb9e4baf85d1ed81c
amirmknv <amir.mukanov@gmail.com>
2023-11-05 19:11:40 +0800

    add even.js

--- src/games/even.js
+++ src/games/even.js
@@ -8,10 +8,28 @@ const getRandomInt =(max) => {
 	return Math.floor(Math.random() * max);
 };
 
+
 export const gameResult = () => {
 	console.log('Answer "yes" if the number is even, otherwise answer "no".');
-	for (let i =0; i < 3; i += 1) {
+	for (let i = 0; i < 3; i += 1) {
 	const number = getRandomInt(100);
 	console.log("Question: " + number);
+	const userAnswer = readlineSync.question("Your answer: ");
+	
+	if (number % 2 === 0 && userAnswer === "yes") {
+		console.log("Correct!")
 	};
+	if  (number % 2 === 0 && userAnswer === "no") {
+		console.log(`${userAnswer} is wrong answer ;\(. Correct answer was ${opposite}.\n Let's try again, ${userName}!`);
+		return;
+	};
+	if (number % 2 !== 0 && userAnswer === "no") {
+		console.log("Correct!")
+	};
+	if (number % 2 !== 0 && userAnswer === "yes") {
+		console.log(`${userAnswer} is wrong answer ;\(. Correct answer was ${opposite}.\n Let's try again, ${userName}!`); 
+		return;
+	};
+	};
+	console.log(`Congratulations, ${userName}!`);
 };
