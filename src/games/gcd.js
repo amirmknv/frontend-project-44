8ecc9dca7d16e83918acbcd065753c9c64499401
amirmknv <amir.mukanov@gmail.com>
2023-11-19 15:02:41 +0800

    brain-gcd.js, gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -7,17 +7,22 @@ const randomNum1 = getRandomInt(1, 50);
 const randomNum2 = getRandomInt(1, 50);
 const question = `Question: ${randomNum1} ${randomNum2}`;
 
-const gameStart = (x, y) => {
- while (y) {
-	 let t = y;
-	 y = x % y;
-	 x = t;
+const answer = (randomNum1, randomNum2) => {
+ while (randomNum1 && randomNum2) {
+if (randomNum1 > randomNum2) {
+   randomNum1 = randomNum1 % randomNum2;
+ }  else {
+   randomNum2 = randomNum2 % randomNum1;
+  } 
  }
+ randomNum1 = randomNum1 + randomNum2;
+ return randomNum1;
 
- let answer = x;
- return [question, answer];
 };
 
+const gameStart = () => {
+ return [question, String(answer(randomNum1, randomNum2))]
+};
 
 export default () => {
 	gameLogic(gameTask, gameStart);
