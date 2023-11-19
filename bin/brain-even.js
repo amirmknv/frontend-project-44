07b281452859d45a9191003af3f65057bc26f965
amirmknv <amir.mukanov@gmail.com>
2023-11-19 09:26:31 +0800

    add brain-even.js, even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
-import { gameResult } from "../src/games/even.js";
-gameResult();
+import gameLogic from "../src/games/even.js";
+gameLogic();
--- src/games/even.js
+++ src/games/even.js
@@ -1,32 +1,18 @@
-import readlineSync from 'readline-sync';
+import gameLogic from "../index.js";
+import getRandomInt from "../getRandomInt.js";
 
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question("May I have your name? ");
-console.log("Hello, " + userName + "!");
 
-const getRandomInt =(max) => {
-	return Math.floor(Math.random() * max);
+
+const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const gameStart = () => {
+const number = getRandomInt(100);
+const question = "Question: " + number;
+const isEven = number % 2 === 0;
+const answer = isEven ? "yes" : "no";
+ return [question, answer];
 };
 
-
-export const gameResult = () => {
-	console.log('Answer "yes" if the number is even, otherwise answer "no".');
-	
-	for (let i = 0; i < 3; i += 1) {
-	const number = getRandomInt(100);
-	console.log("Question: " + number);
-	const userAnswer = readlineSync.question("Your answer: ");
-	const isEven = number % 2 === 0;
-	const correctAnswer = isEven ? "yes" : "no";
-	if (correctAnswer !== userAnswer) {
-	 console.log(`${userAnswer} is wrong answer ;\(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
-	return;
-	}
-
-	console.log("Correct!");
-	}
-
-
-	console.log(`Congratulations, ${userName}!`);
-	
+export default () => {
+	gameLogic(gameTask, gameStart); 
 };
