f7aa84869d8308c0de8605e8b830c7253ca3b9d9
amirmknv <amir.mukanov@gmail.com>
2023-11-20 20:24:22 +0800

    add gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,9 +3,8 @@ import getRandomInt from "../getRandomInt.js";
 
 const gameTask = "Find the greatest common divisor of given numbers.";
 
-const randomNum1 = getRandomInt(1, 50);
-const randomNum2 = getRandomInt(1, 50);
-const question = `Question: ${randomNum1} ${randomNum2}`;
+
+
 
 const answer = (randomNum1, randomNum2) => {
  while (randomNum1 && randomNum2) {
@@ -21,7 +20,10 @@ if (randomNum1 > randomNum2) {
 };
 
 const gameStart = () => {
- return [question, String(answer(randomNum1, randomNum2))]
+const randomNum1 = getRandomInt(1, 50);
+const randomNum2 = getRandomInt(1, 50);
+const question = `Question: ${randomNum1} ${randomNum2}`;
+return [question, String(answer(randomNum1, randomNum2))]
 };
 
 export default () => {
