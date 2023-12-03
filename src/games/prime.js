8fbd4f9187fdfaae1e8a49ecf2c69f70b5dbdfbd
amirmknv <amir.mukanov@gmail.com>
2023-12-03 13:57:20 +0800

    add eslintrc.yml, prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,16 +5,16 @@ const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 const isPrime = (num) => {
   if (num < 2) {
-  }
+  
   return false;
-};
-
+}
 for (let i = 2; i <= Math.sqrt(num); i++) {
   if (num % i === 0) {
     return false;
    }
 }
  return true;
+};
 
 const gameStart = () => {
   const question = getRandomInt(1, 100);
