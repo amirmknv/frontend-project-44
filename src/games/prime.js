8b24786da9e598ec5303a6c012bcc010643788b8
amirmknv <amir.mukanov@gmail.com>
2023-12-03 08:40:40 +0800

    add prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,10 +5,14 @@ const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 const gameStart = () => {
   const isPrime = (num) => {
-  if (num < 2) return false;
+  if (num < 2) {
+   return false;
+    }
 
   for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return false;
+    if (num % i === 0) {
+    return false;
+   }
   }
 
   return true;
