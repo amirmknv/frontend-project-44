93470227af6759934559b33bb64384d90af509a9
amirmknv <amir.mukanov@gmail.com>
2023-12-03 13:41:52 +0800

    add .eslint.yml, package-lock.json, package.json, prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,28 +3,25 @@ import getRandomInt from "../getRandomInt.js";
 
 const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
- const isPrime = (num) => {
+const isPrime = (num) => {
   if (num < 2) {
-   return false;
-    }
-
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-    return false;
-   }
   }
-
-  return true;
- }; 
- 
- const gameStart = () => {
- const question = getRandomInt(1, 100);
- const answer = isPrime(question) ? "yes" : "no";
- return [question, answer];
- 
+  return false;
 };
 
+for (let i = 2; i <= Math.sqrt(num); i++) {
+  if (num % i === 0) {
+    return false;
+   }
+}
+ return true;
+
+const gameStart = () => {
+  const question = getRandomInt(1, 100);
+  const answer = isPrime(question) ? "yes" : "no";
+  return [question, answer];
+};
 
 export default () => {
-	gameLogic(gameTask, gameStart);
+  gameLogic(gameTask, gameStart);
 };
