f443f06d6a0e114e085fd3f04412ac16b4127a2c
amirmknv <amir.mukanov@gmail.com>
2024-02-05 13:12:53 +0800

    add cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,6 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name?');
 
-// eslint-disable-next-line no-unused-vars
 console.log(`Hello, ${userName}!`);
 
 export default userName;
